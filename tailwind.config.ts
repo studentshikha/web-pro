4d04b22a28d3cc1b3dacb582a0d343d515067db7
studentshikha <shikhasinghchauhan28@gmail.com>
2024-12-10 17:15:26 +0530

    commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,7 +13,7 @@ export default {
       },  keyframes: {
         fadeIn: {
                 '0%': { opacity: '0', transform: 'translateY(10px)' }, 
-        '100%': { opacity: '1', transform: 'translateY(0)'},git 
+        '100%': { opacity: '1', transform: 'translateY(0)'},
         },
       },
       colors: {
