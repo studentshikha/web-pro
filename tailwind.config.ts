88ead098774601f65b2a92466a000fd385d3d3c7
studentshikha <shikhasinghchauhan28@gmail.com>
2024-12-10 17:09:28 +0530

    jdk

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,8 +12,8 @@ export default {
         fadeIn: 'fadeIn 3s ease-out forwards',
       },  keyframes: {
         fadeIn: {
-          '0%': { opacity : 0, transform: 'translateY(10px)' },
-          '100%': { opacity : 1, transform: 'translateY(0)' },
+                '0%': { opacity: '0', transform: 'translateY(10px)' }, 
+        '100%': { opacity: '1', transform: 'translateY(0)'},git 
         },
       },
       colors: {
